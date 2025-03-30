79125171c2bff76d37d8fa32b8856bae032de65a
cluffa <cluffa@users.noreply.github.com>
2025-03-29 22:09:42 -0400

    Remove redundant user refresh call in activity download loop

--- .github/workflows/download_all.jl
+++ .github/workflows/download_all.jl
@@ -7,11 +7,9 @@ Pkg.add(url = "https://github.com/cluffa/StravaConnect.jl.git")
 using StravaConnect
 
 u = setup_user();
-refreresh_if_needed!(u)
 acts = get_activity_list(u)
 
 for act in acts[1:50]
-    refreresh_if_needed!(u)
     full_activity = get_activity(act[:id], u)
     @info "Downloaded activity ID: $(act[:id])"
 end
