f725aa1c99c68ebfc088d9b5132318c10d1e5fff
cluffa <cluffa@users.noreply.github.com>
2025-03-29 22:03:48 -0400

    Limit activity download to the first 50 entries and refresh user data as needed

--- .github/workflows/download_all.jl
+++ .github/workflows/download_all.jl
@@ -7,9 +7,11 @@ Pkg.add(url = "https://github.com/cluffa/StravaConnect.jl.git")
 using StravaConnect
 
 u = setup_user();
+refreresh_if_needed!(u)
 acts = get_activity_list(u)
 
-for act in acts
+for act in acts[1:50]
+    refreresh_if_needed!(u)
     full_activity = get_activity(act[:id], u)
     @info "Downloaded activity ID: $(act[:id])"
 end
